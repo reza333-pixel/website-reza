55e22dd019c65ac58d2c87572486b2e2662e1b3c
reza333-pixel <64386436+reza333-pixel@users.noreply.github.com>
2024-10-17 16:51:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Script untuk interaksi (opsional)
+document.querySelectorAll('.service-button').forEach(button => {
+    button.addEventListener('', () => {
+        alert('wait!'); // Ganti dengan interaksi yang sesuai
+    });
+});
